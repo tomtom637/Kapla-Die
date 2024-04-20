bfb075268ec7c81ec6a0e46200d33ff51a1b9fde
Thomas Lacombe <tomtom637@gmail.com>
2024-04-20 12:41:52 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,93 @@
+import { useState, useRef, useEffect } from "react";
+import { Button } from "./components/ui/button";
+
+const colors = ["pink", "blue", "green", "yellow", "red", "white"] as const;
+
+type Color = (typeof colors)[number];
+
+const pickRandomColor = () => colors[Math.floor(Math.random() * colors.length)];
+
+const colorsClasses = {
+  pink: "bg-pink-500",
+  blue: "bg-blue-500",
+  green: "bg-green-500",
+  yellow: "bg-yellow-500",
+  red: "bg-red-500",
+  white: "bg-white",
+} satisfies Record<Color, string>;
+
+const ColorCell = ({ color, isLast }: { color: Color; isLast: boolean }) => (
+  <div
+    className={`${colorsClasses[color]} ${isLast ? "animate-pulse" : ""} rounded-sm  p-2 text-center text-black`}
+  >
+    {color}
+  </div>
+);
+
+type ColorState = {
+  id: number;
+  color: Color;
+};
+
+export default function App() {
+  const [colors, setColors] = useState<ColorState[]>([]);
+  const colorsRef = useRef<HTMLDivElement>(null);
+
+  const handleReset = () => setColors([]);
+
+  const handleClick = () => {
+    setColors((prev) => [
+      ...prev,
+      { id: prev.length + 1, color: pickRandomColor() },
+    ]);
+  };
+
+  useEffect(() => {
+    if (colorsRef.current && colors.length > 0) {
+      colorsRef.current?.scrollTo({
+        top: colorsRef.current.scrollHeight,
+        behavior: "smooth",
+      });
+    }
+  }, [colors]);
+
+  return (
+    <div className="min-h-dvh bg-gradient-to-tr from-slate-900 to-slate-800 text-slate-100">
+      <header className="mx-auto flex max-w-xl items-baseline justify-between p-4">
+        <div>
+          <Button
+            className="size-[2.25rem] rounded-full bg-red-500 p-1 text-xs italic text-slate-900 hover:bg-red-400"
+            onClick={handleReset}
+          >
+            Reset
+          </Button>
+        </div>
+        <div>
+          <Button
+            onClick={handleClick}
+            className="rounded-full bg-slate-600 hover:bg-slate-500"
+          >
+            Pick a color
+          </Button>
+        </div>
+        <div>
+          <span className="block min-w-[3.6rem] text-end font-mono text-xs italic text-yellow-400">
+            {colors.length} {colors.length === 1 ? "turn" : "turns"}
+          </span>
+        </div>
+      </header>
+      <div
+        ref={colorsRef}
+        className="mx-auto grid max-h-[37.5rem] max-w-xl grid-cols-[repeat(auto-fill,minmax(90px,1fr))]  gap-4 overflow-y-scroll p-4"
+      >
+        {colors.map((color, index) => (
+          <ColorCell
+            key={color.id}
+            color={color.color}
+            isLast={index === colors.length - 1}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ vite.config.ts
@@ -0,0 +1,40 @@
+import path from "node:path";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["vite.svg"],
+      manifest: {
+        name: "Kapla die",
+        short_name: "Kapla die",
+        scope: "/",
+        description: "A dice to play colored Kapla game",
+        display: "standalone",
+        theme_color: "#000",
+        icons: [
+          {
+            src: "192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+  base: "/Kapla/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
